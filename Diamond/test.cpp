932e9a5771a663c6f7030bdc36e3342cab7b3fa7
Hugo Traverson <hugo.traverson@ubisoft.com>
2024-06-16 10:37:13 +0200

    [rework-dojo] Creating bot face of Diamond
    * Adding parametrized test for bot face of diamond 'A', 'B' and 'C'

--- Diamond/test.cpp
+++ Diamond/test.cpp
@@ -58,10 +58,10 @@ void create_diamond_top(std::stringstream& oss, char currentLetter, char diamond
 	}
 }
 
-class CreateDiamondTestFixture
+class CreateDiamondTopTestFixture
 	: public ::testing::TestWithParam<std::tuple<char, std::string>> {};
 
-TEST_P(CreateDiamondTestFixture, CreateDiamondAndTestTopFace)
+TEST_P(CreateDiamondTopTestFixture, CreateDiamondAndTestTopFace)
 {
 	std::stringstream oss;
 	auto& param = GetParam();
@@ -75,10 +75,52 @@ TEST_P(CreateDiamondTestFixture, CreateDiamondAndTestTopFace)
 
 INSTANTIATE_TEST_CASE_P(
 	DiamondTestSuite,
-	CreateDiamondTestFixture,
+	CreateDiamondTopTestFixture,
 	::testing::Values(
 		std::make_tuple('A', "A"),
 		std::make_tuple('B', " A\nB B"),
 		std::make_tuple('C', "  A\n B B\nC   C")
+	)
+);
+
+void create_diamond_bot(std::stringstream& oss, char currentLetter, char diamondCenterLetter)
+{
+	static constexpr char BASE_A = 'A';
+	if (currentLetter < BASE_A) return;
+
+	int n = diamondCenterLetter - BASE_A;
+	int currentPos = currentLetter - BASE_A;
+
+	add_diamond_line(oss, currentLetter, n - currentPos, 2 * currentPos - 1);
+
+	if (--currentLetter >= BASE_A)
+	{
+		oss << '\n';
+		create_diamond_bot(oss, currentLetter, diamondCenterLetter);
+	}
+}
+
+class CreateDiamondBotTestFixture
+	: public ::testing::TestWithParam<std::tuple<char, std::string>> {};
+
+TEST_P(CreateDiamondBotTestFixture, CreateDiamondAndTestBotFace)
+{
+	std::stringstream oss;
+	auto& param = GetParam();
+
+	char diamondCenterLetter = std::get<0>(param);
+	std::string expected = std::get<1>(param);
+
+	create_diamond_bot(oss, diamondCenterLetter, diamondCenterLetter);
+	EXPECT_EQ(expected, oss.str());
+}
+
+INSTANTIATE_TEST_CASE_P(
+	DiamondTestSuite,
+	CreateDiamondBotTestFixture,
+	::testing::Values(
+		std::make_tuple('A', "A"),
+		std::make_tuple('B', "B B\n A"),
+		std::make_tuple('C', "C   C\n B B\n  A")
 	)
 );
